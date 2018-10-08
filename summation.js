537545329339701f8dd6e2679f6669690a4b1e9e
andrew gibbs <andrewgibbs@outlook.com>
2018-10-08 09:54:58 -0700

    first commit

---
+++ summation.js
@@ -0,0 +1,22 @@
+// use strict
+// summation returns to summation of an integer.
+const summation = (num) => {
+  if (typeof(num) === 'number'){
+    try {
+      //Base cases
+      if (num <= 0) return 0;
+      if(num === 0) return 1;
+      // The recursive call
+      return num + summation(num - 1);
+    }
+    catch (err) {
+      return err;
+    }
+  } else {
+      return 'Not A number';
+    }
+}
+
+console.log(summation(4));
+console.log(summation(12));
+console.log(summation('cat'));
