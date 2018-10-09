894f483ed11e46272ef99274fd780e0b370739b5
andrew gibbs <andrewgibbs@outlook.com>
2018-10-09 04:24:49 -0700

    feat: Combination makes use of factorial function

--- combination.js
+++ combination.js
@@ -1,15 +1,12 @@
 // use strict
 // Combination returns to summation of an integer.
 
-const combination = (population, sample_size) => {
-  if ((typeof(population) === 'number') && (typeof(sample_size) === 'number')){
-    try {
-      //Base cases
-      if(population === 0) return 1;
-      if(population === sample_size) return 1;
+let {factorial} = require('./factorial');
 
-      // The recursive call
-      return population * combination(population-1,sample_size);
+const combination = (population, sample_size) => {
+  if (typeof(population) === 'number' && typeof(sample_size) === 'number' ){
+    try {
+      return factorial(population) / factorial(population - sample_size)
     }
     catch (err) {
       return err;
